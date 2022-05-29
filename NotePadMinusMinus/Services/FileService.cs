dafd0b0712eae2cbd73dd00046e99eb12e917b65
Yahya Önder <06yahya06@gmail.com>
2022-05-29 23:30:12 +0300

    Add project files.

---
+++ NotePadMinusMinus/Services/FileService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePadMinusMinus.Services
+{
+    public class FileService
+    {
+        public string Open(string path)
+        {
+            string filePath = path;
+            return File.ReadAllText(filePath);
+        }
+
+        public void Save(string path, string content)
+        {
+            File.WriteAllText(path, content);
+        }
+
+        public string GetTextLength(string text)
+        {
+            return text.Length.ToString();
+        }
+    }
+}
